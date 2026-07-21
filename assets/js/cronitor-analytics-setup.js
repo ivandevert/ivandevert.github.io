820c272cae9327a768c033ce741ea2721bbdf72d
ivandevert <ivandevert@gmail.com>
2026-07-21 23:18:55 +0000

    Deploying to gh-pages from @ ivandevert/ivandevert.github.io@aac85cdb62dba251dd3070b6f924850c54c40da3 🚀

--- assets/js/cronitor-analytics-setup.js
+++
@@ -1,6 +0,0 @@
-window.cronitor =
-  window.cronitor ||
-  function () {
-    (window.cronitor.q = window.cronitor.q || []).push(arguments);
-  };
-cronitor("config", { clientKey: "5e252c8d2c4a5132662b0da6a4bbd281" });
